6ea75520ca67a5761b5fd542a07f3f1fb36636e8
Mr Mixailov <Mr_Mixailov@yandex.ru>
2021-11-22 23:24:16 +0300

    add HelloCode-2

---
+++ padanie3/Program.cs
@@ -0,0 +1,14 @@
+﻿int a = 50;
+int b = 100;
+
+if (a>b)
+{
+    Console.WriteLine("Max = " + a);
+    Console.WriteLine("Max = " + b);
+
+}
+else 
+{
+    Console.WriteLine("Max = " + b);
+    Console.WriteLine("Max = " + a);
+}
---
+++ zadaine4/Program.cs
@@ -0,0 +1,55 @@
+﻿//По заданному номеру дня недели вывести его название
+//Console.Write("Введите число: ");
+//int num = Convert.ToInt16(Console.ReadLine());
+//switch (num)
+//{
+ //   case 1: Console.WriteLine("Понедельник"); break;
+   // case 2: Console.WriteLine("Вторник"); break;
+  //  case 3: Console.WriteLine("Среда"); break;
+   // case 4: Console.WriteLine("Четверг"); break;
+  //  case 5: Console.WriteLine("Пятница"); break;
+ //   case 6: Console.WriteLine("Суббота"); break;
+ //   case 7: Console.WriteLine("Воскресенье"); break;
+  //  default: Console.WriteLine("ERROR"); break;
+//}
+//Console.ReadKey();
+Console.WriteLine("Введите число от 1 до 7 ");
+string number = Console.ReadLine(); 
+int day = Convert.ToInt16(number);
+
+
+if (day == 1)
+{
+ Console.WriteLine("Понедельник");
+}
+if (day == 2)
+{
+ Console.WriteLine("вторник");
+}
+if (day == 3)
+{
+ Console.WriteLine("Среда");
+}
+if (day == 4)
+{
+ Console.WriteLine("Четверг");
+}
+if (day == 5)
+{
+ Console.WriteLine("Пятница");
+}
+if (day == 6)
+{
+ Console.WriteLine("суббота");
+}
+if (day == 7)
+{
+ Console.WriteLine("Всокресенье");
+}
+
+
+
+
+
+
+
